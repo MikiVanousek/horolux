069608e1031cf19a29195cb7373dc0b8da5af809
Miki Vanusek <vanousekmikulas@gmail.com>
2022-02-07 11:52:24 +0100

    SettingsScreen: improved colors

--- lib/presets/hl_colors.dart
+++ lib/presets/hl_colors.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 
 class HLColors {
   static const Color background = Colors.black,
-  accent = Colors.white,
-  accentDull = Color(0xFFBCBCBC);
-  
+      accent = Colors.white,
+      accentDull = Color(0xFFBCBCBC);
+
   static final materialTheme = ThemeData(
-    backgroundColor: background,
-    
+    // time picker background
+    cardColor: accentDull,
+    colorScheme: ColorScheme.dark(
+      primary: accent,
+      secondary: background,
+    ),
   );
 }
